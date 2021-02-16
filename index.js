6b4b94c7cc7ff93c6643015c8efbf95f60848ed8
Joshua Ordaz <joshuaordaz@comcast.net>
2021-02-15 18:38:21 -0800

    create init function and add function

--- index.js
+++ index.js
@@ -14,10 +14,55 @@ const connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId + "\n");
-  placeholderfunction();
+  manageCompany();
 });
 
-function placeholderfunction() {
-    
+manageCompany = () => {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "chooseAction",
+            message: "What would you like to do?",
+            choices: [
+                "Add department, role, or employee",
+                "View departments, roles, or employees",
+                "Update departments, roles, or employees",
+                "Exit"
+            ]
+        }]).then(selection => {
+        if (selection.chooseAction === "Add department, role, or employee") {
+            add();
+        } else if (selection.chooseAction === "View departments, roles, or employees") {
+            view();
+        } else if (selection.chooseAction === "Update departments, roles, or employees") {
+            update();
+        } else {
+            quit();
+        }
+    });
+}
 
+add = () => {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "chooseAdd",
+            message: "What would you like to add?",
+            choices: [
+                "Add department",
+                "Add role",
+                "Add employee",
+                "Main Menu"
+            ]
+        }]).then(selection => {
+            if (selection.chooseAction === "Add department") {
+                addDept();
+            } else if (selection.chooseAction === "Add role") {
+                addRole();
+            } else if (selection.chooseAction === "Add employee") {
+                addEmployee();
+            } else {
+                manageCompany();
+            }
+    });
 }

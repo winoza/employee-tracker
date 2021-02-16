2fb708e22c2387676750d59fe2f3c3d45b6536bb
Joshua Ordaz <joshuaordaz@comcast.net>
2021-02-15 17:19:59 -0800

    fix package json and update schema & seed

--- schema.sql
+++ schema.sql
@@ -3,3 +3,25 @@ CREATE DATABASE employee_management_db;
 
 USE employee_management_db;
 
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    department_name VARCHAR(35) NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE employee_role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(45) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE table employee (    
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(45) NOT NULL
+    last_name VARCHAR(45) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NOT NULL,
+    PRIMARY KEY (id)
+);
--- seed.sql
+++ seed.sql
@@ -1,1 +1,44 @@
 USE employee_management_db;
+
+INSERT INTO department (department_name) VALUES
+    ('Operations'),
+    ('Research and Development'),
+    ('Human Resources'),
+    ('Legal'),
+    ('Sales'),
+    ('Finance'),
+    ('IT');
+
+
+INSERT INTO employee_role (title, salary, department_id) VALUES
+    ('Operations Analyst', 80000, 1),
+    ('Operations Manager', 100000, 1),
+    ('Research Analyst', 85000, 2),
+    ('Research Manager', 100000, 2),
+    ('HR Analyst', 80000, 3),
+    ('HR Manager', 100000, 3),
+    ('Contract Lawyer', 140000, 4),
+    ('Compliance and Legal Manager', 180000, 4),
+    ('Salesperson', 80000, 5),
+    ('Sales Lead', 100000, 5),
+    ('Senior Finance Advisor', 110000, 6),
+    ('Director of Finance', 150000, 6),
+    ('IT Technician', 98000, 7),
+    ('System Admin', 135000, 7),
+    
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
+    ('Wiz', 'Khalifa', 1, NULL),
+    ('Snoop', 'Dogg', 2, 1),
+    ('Juicy', 'J', 3, NULL),
+    ('Jay', 'Z', 4, 2),
+    ('Kanye', 'West', 5, NULL),
+    ('Kendrick', 'Lamar', 6, 3),
+    ('50', 'Cent', 7, NULL),
+    ('Tierra', 'Whack', 8, 4);
+    ('Lauryn', 'Hill', 9, NULL),
+    ('Young', 'Thug', 10, NULL),
+    ('MF', 'Doom', 11, NULL),
+    ('Joey', 'Bada$$', 12, NULL),
+    ('Capital', 'Steez', 13, NULL),
+    ('Lupe', 'Fiasco', 14, NULL),
